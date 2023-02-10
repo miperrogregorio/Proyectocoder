6ca498dd6972064dfa69268a38ed8ad12dceb2d9
DIego Luis Romero <Dr@MacBook-Air-de-LiliT.local>
2023-02-10 00:13:44 -0300

    otra nueva mod

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,16 +5,21 @@ from AppCoder.models import Curso, Estudiantes, Profesor, Entregable
 
 
 def inicio(request):
-   return HttpResponse("Vista inicio")
+   #return HttpResponse("Vista inicio")
+   return render(request, 'AppCoder/inicio.html')
 
 def curso(request):
-   return HttpResponse("Vista cursos")
+   #return HttpResponse("Vista cursos")
+    return render(request, 'AppCoder/cursos.html')
 
 def profesores(request):
-   return HttpResponse("Vista prefesores")
+   #return HttpResponse("Vista prefesores")
+    return render(request, 'AppCoder/profesores.html')
 
 def entregables(request):
-   return HttpResponse("Vista entregables")  
+    return render(request, 'AppCoder/entregables.html')
+   #return HttpResponse("Vista entregables")  
 
 def estudiantes(request):
-   return HttpResponse("Vista estudiantes")   
+   #return HttpResponse("Vista estudiantes")
+   return render(request, 'AppCoder/estudiantes.html')   
